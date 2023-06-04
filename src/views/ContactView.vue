0d7a7bcb4a4ad3ae51e7db75375ed4bd1be2d04a
Arthur de la Po <arthurdelapo@gmail.com>
2023-06-05 02:26:59 +0300

    add: contact link

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -3,9 +3,21 @@
     <div class="hero min-h-screen">
       <div class="hero-content text-center flex flex-col">
         <h1 class="text-5xl">Contact</h1>
-        <div>https://twitter.com/6of6Club</div>
-        <div>https://github.com/6of6Club</div>
-        <div>team@6of6.club</div>
+        <div>
+          <a href="https://github.com/6of6Club" target="_blank"
+            >https://github.com/6of6Club</a
+          >
+        </div>
+        <div>
+          <a href="https://twitter.com/6of6Club" target="_blank"
+            >https://twitter.com/6of6Club</a
+          >
+        </div>
+        <div>
+          <a href="https://youtube.com/@6of6Club" target="_blank"
+            >https://youtube.com/@6of6Club</a
+          >
+        </div>
       </div>
     </div>
   </div>
